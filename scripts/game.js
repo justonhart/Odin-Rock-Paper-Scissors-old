7e57f1e24ccaac9f5c3f13fc4c7214e978d6593e
Juston Hart <contact@justonh.art>
2020-04-07 17:20:59 -0500

    Add score tracking, replay option

--- scripts/game.js
+++ scripts/game.js
@@ -1,32 +1,48 @@
 
 function playRound(playerChoice, computerChoice){
-    let output;
+    let output, winner;
     
     if(playerChoice === computerChoice)
         output =  "It's a draw!";
     
     else{
         if(playerChoice == "rock"){
-            if(computerChoice == "paper")
+            if(computerChoice == "paper"){
                 output = "You lose! Paper beats Rock!";
-            else
+                updateScores("c");
+            }
+            else{
                 output = "You win! Rock beats Paper!";
+                updateScores("p");
+            }
+                
         }
         else if(playerChoice == "paper"){
-            if(computerChoice == "scissors")
+            if(computerChoice == "scissors"){
                 output = "You lose! Scissors beats Paper!";
-            else   
+                updateScores("c");
+            }
+            else{
                 output = "You win! Paper beats Rock!";
+                updateScores("p");
+            }
+                
         }
         else{
-            if(computerChoice == "rock")
+            if(computerChoice == "rock"){
                 output = "You lose! Rock beats Scissors!";
-            else
+                updateScores("c");
+            }
+            else{
                 output = "You win! Scissors beats Paper!";
+                updateScores("p");
+            }
         }
     }
 
     document.getElementById("matchResult").innerHTML = output;
+    if(winner)
+        updateScores();
     return;
 }
 
@@ -56,4 +72,31 @@ function game(){
     }
 
     console.log(`You won ${playerWins} times! Computer won ${computerWins} times!`);
+}
+
+function updateScores(winner){
+    let playerWins = Number(document.getElementById("playerScore").innerHTML.slice(-1));
+    let computerWins = Number(document.getElementById("computerScore").innerHTML.slice(-1));
+
+    //increment the winner's score
+    if(winner === "c")
+        document.getElementById("computerScore").innerHTML = `Computer score: ${++computerWins}`;
+    else
+        document.getElementById("playerScore").innerHTML = `Player score: ${++playerWins}`;
+
+    //best of 5
+    if(playerWins == 3 || computerWins == 3)
+        offerReplay();
+
+    return;
+}
+
+//once a score of 3 is reached, check to see if the player wants to restart
+function offerReplay(){
+    document.getElementById("buttonPanel").setAttribute("hidden", true);
+    document.getElementById("replay").removeAttribute('hidden');
+}
+
+function reset(){
+
 }
